3adba5395dd5d56a6161228507b1700123afe2dc
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-06 11:36:05 +0530

    changes made inside file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import Users from './Users';
+import React,{ useState } from 'react';
+
 import Students from './Students';
 
 // this is use when you direct export component
@@ -9,43 +9,14 @@ import Students from './Students';
 
 function App() {
   // useState=using for stored the data.
-  const [data,setData]=useState(0);
-
-  function updateData(){
-    // window.confirm("Are you sure to updated data?");
-
-    if(window.confirm("Are you sure to update data?")===true){
-      setData(data+1);
-    }
-
-    
-  }
-  // console.log("-----------------------------");
+  const [name,setData]=useState("Nishant");    
   return (
     <div className='App'>
-      <h1>Count {data}</h1>
-      <button onClick={()=>alert("hi")}>Click me Second</button>
-      <button onClick={updateData}>Update data</button>
-      <Users/>
-      <Students name={"Abhishek"} email={"abhishek.tiwari@firsthive.com"} others={{address:"U.P.",mobile:"555-555-5555"}}/>
-      <Students name={"Nishant"} email={"nishant.bhatnagar@firsthive.com"} others={{address:"U.P.",mobile:"666-666-6666"}}/>
-      <Students name={"Ketan"} email={"ketan.suryavansi@firsthive.com"} others={{address:"M.P.",mobile:"888-444-3333"}}/>
+      <Students name={name} email={"abhishek.tiwari@firsthive.com"} others={{address:"U.P.",mobile:"555-555-5555"}}/>
+      <button onClick={()=>{setData('Abhishek')}}>Click updateNewData</button>
     </div>
   ); 
 }
-
-
-
-// Here you can create component in same page
-// const hstyle={color:'green'};
-// const Users = ()=>{
-//   return(
-//       <>
-//           <h1 style={hstyle}>Hello User welcome to react world!</h1>
-//       </>
-//   );
-// }
-
 
 
 export default App;
