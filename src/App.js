c94e68cebccd94acef012ce067697e154732ed83
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-07 12:36:08 +0530

    Basic Form validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
-import Profile from './Profile';
 function App() {
   return (
     <div className='App'>
-      <Profile/>      
+      <Login/>      
     </div>  
   );
 }
