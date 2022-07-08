92486433f0c8a680ed8060492e2bcc3bfc3dbab6
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-08 17:02:40 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 
 
 const  App=()=> {
-  const [data,setData]=useState("Nishant");
+  const [count,setCount]=useState(0);
+  useEffect(()=>{
+    console.log("useeffect");
+  })
   return(
     <div>
-      <h1>Hello {data}</h1>
-      <button onClick={()=>{
-        setData("Ketan");
-      }}>Update Name</button>
+      <h1>Hello {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Update Count</button>
     </div>
   );
 }
