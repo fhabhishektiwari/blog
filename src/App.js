55b1951adbe5f7ca6e3fa96eeb491e7f1a300531
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-12 11:38:30 +0530

    added Nested list in reactjs

--- src/App.js
+++ src/App.js
@@ -1,43 +1,97 @@
 // import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const users = [
-    { name: 'Nishant', email: 'nishant@gmail.com', mobileno: '111' },
-    { name: 'Ketan', email: 'ketan@gmail.com', mobileno: '111' },
-    { name: 'Gaurav', email: 'gaurav@gmail.com', mobileno: '111' },
-    { name: 'Yashwant', email: 'yashwant@gmail.com', mobileno: '101' },
-    { name: 'Ankur', email: 'ankur@gmail.com', mobileno: '111' }
+    {
+      name: 'Nishant', email: 'nishant@gmail.com', address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "34", city: "Gurgaon", country: "India" },
+        { Hn: "43", city: "Delhi", country: "India" },
+        { Hn: "63", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: 'Ketan', email: 'ketan@gmail.com', address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "34", city: "Gurgaon", country: "India" },
+        { Hn: "43", city: "Delhi", country: "India" },
+        { Hn: "63", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: 'Gaurav', email: 'gaurav@gmail.com', address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "34", city: "Gurgaon", country: "India" },
+        { Hn: "43", city: "Delhi", country: "India" },
+        { Hn: "63", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: 'Yashwant', email: 'yashwant@gmail.com', address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "34", city: "Gurgaon", country: "India" },
+        { Hn: "43", city: "Delhi", country: "India" },
+        { Hn: "63", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: 'Ankur', email: 'ankur@gmail.com', address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "34", city: "Gurgaon", country: "India" },
+        { Hn: "43", city: "Delhi", country: "India" },
+        { Hn: "63", city: "Noida", country: "India" }
+      ]
+    }
 
   ]
   return (
     <div className='App'>
-      <Table border={1} striped bordered hover variant="light">
+      <h1>List with nested Array</h1>
+
+      <Table variant='dark' striped hover>
         <thead>
           <tr>
             <th>Id</th>
             <th>Name</th>
             <th>Email</th>
-            <th>Mobile Number</th>
+            <th>Address</th>
           </tr>
         </thead>
         <tbody>
-        {
-          users.map((data,key)=>
-          data.mobileno==='101'?
-          <tr key={key}>
-            <td>{key+1}</td>
-            <td>{data.name}</td>
-            <td>{data.email}</td>
-            <td>{data.mobileno}</td>
-          </tr>:null
-          )
-        }
+          {
+            users.map((data, key) => {
+              return <tr key={key}>
+                <td>{key + 1}</td>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>
+                  <Table variant='dark' striped hover borderless>
+                    <tbody>  
+                  {
+                      data.address.map((ad, key) => {
+                        return <tr key={key} style={{ color: '#fff' }}>
+                          <td>{ad.Hn}</td>
+                          <td>{ad.city}</td>
+                          <td>{ad.country}</td>
+                        </tr>
+                      })
+
+                    }
+                    </tbody>
+                  </Table>
+                </td>
+              </tr>
+            })
+          }
+
         </tbody>
+
       </Table>
+
     </div>
   );
 }
