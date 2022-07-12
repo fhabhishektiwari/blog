8168243584c8980c32cd9c2deb665812086a5b4a
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-12 17:22:11 +0530

    useRef in react js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import './App.css'
-import React,{createRef} from 'react';
-class App extends React.Component{
-  constructor(){
-    super();
-    this.inputRef=createRef();
+import React,{useRef} from 'react';
+const App=()=>{
+  let inputRef=useRef(null);
+  function handleInput(){
+    // console.log("clicked")
+    inputRef.current.value=1000;
+    inputRef.current.focus();
+    inputRef.current.style.color="red";
   }
-  componentDidMount(){
-    // console.warn(this.inputRef.current.value="100000");
-    this.inputRef.current.style.color="red";
-    this.inputRef.current.style.backgroundColor="#000";
-  }
+  return(
+    <div className='App'>
+      <h1>useRef In React js</h1>
+      <input type='text' ref={inputRef}/>
+      <button onClick={()=>{handleInput()}}>Handle Input</button>
+    </div>
+  ); 
 
-  getVal(){
-    console.warn(this.inputRef.current.value);
-    this.inputRef.current.style.color="red";
-    this.inputRef.current.style.backgroundColor="#000";
-  }
-  render(){
-    return(
-      <div className='App'>
-        <h1>Ref in React</h1>
-        <input type='text' ref={this.inputRef}/>
-        <button onClick={()=>this.getVal()}>Check Ref</button>
-      </div>
-  
-    );
-  }
-  
 }
 
 export default App;
