b3d0cfc8a17ffc6417eb14128592f891b48ffdb9
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-13 14:44:33 +0530

    React Router,Link,Navbar,anchor Tag in react js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
+import NavBar from './components/NavBar';
 const App=()=>{
   return(
     <div className='App'>
     <BrowserRouter>
+    {/*<Link to='/about'>About</Link><br/>
+  <Link to='/'>Home</Link>*/}
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
