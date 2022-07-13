11335776f2d990ce887f838c34cb563548ced8d2
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-13 11:03:22 +0530

    controlled component in react js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css'
-import React,{useRef} from 'react';
-import NewChildComponent from './NewChildComponent';
+import React,{useState} from 'react';
 const App=()=>{
-  let inputRef=useRef(null);
-  function updateInput(){
-    inputRef.current.value="1000";
-    inputRef.current.style.color="red";
-    inputRef.current.focus();
-  }
+  const [val,setVal]=useState("0");//default value
+  const [item,setItem]=useState("0");
   return(
     <div className='App'>
-      <h1>forwordRef In React js</h1>
-      <NewChildComponent ref={inputRef}/>
-      <button onClick={updateInput}>Update InputBox</button>
+      <h1>Controlled Component</h1>
+      {/* you can't modify the value*/}
+    {/*<input type='text' value="100"/>*/}
+    <input type='text' value={val} onChange={(e)=>setVal(e.target.value)}/>
+    <input type='text' value={item} onChange={(e)=>setItem(e.target.value)}/>
+
+    <h3>Value: {val}</h3>
+    <h3>Item: {item}</h3>
+
     </div>
   ); 
 
