5ad26e331ca6d73ad197928143cae24f0d6922f3
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-14 16:09:18 +0530

    Active link style

--- src/components/NewNavbar.js
+++ src/components/NewNavbar.js
@@ -2,11 +2,12 @@ import { NavLink } from "react-router-dom";
 import '../App.css'
 const NewNavbar=()=>{
     return(
-        <div>
+        <div className="main-nav">
             <ul className="navbar">
-                <li><NavLink className='nav-bar-link' to='/'>Home</NavLink></li>
-                <li><NavLink className='nav-bar-link' to='/about'>About</NavLink></li>
-                <li><NavLink className='nav-bar-link' to='/contact'>Contact Us</NavLink></li>           
+                {/* 2nd methods to make active  style={({isActive})=>{return {color:isActive?'#0b7753':'#000'} && {borderBottom:isActive?'2px solid #ff6f00':''}}}*/}
+                <li><NavLink className='nav-bar-link' to='/' style={({isActive})=>{return {color:isActive?'#fff':''} && {borderBottom:isActive?'2px solid #ff6f00':''}}}>Home</NavLink></li>
+                <li><NavLink className='nav-bar-link' to='/about' style={({isActive})=>{return {color:isActive?'#0b7753':''} && {borderBottom:isActive?'2px solid #ff6f00':''}}}>About</NavLink></li>
+                <li><NavLink className='nav-bar-link' to='/contact' style={({isActive})=>{return {color:isActive?'#0b7753':'#000'} && {borderBottom:isActive?'2px solid #ff6f00':''}}}>Contact Us</NavLink></li>           
             </ul>
         </div>
     );
