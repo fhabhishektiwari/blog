7ac55ccacd097ebda06fde42c6d27889e1e4fc1d
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-15 14:16:28 +0530

    useLocation in react js

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
+import { useParams,useLocation } from "react-router-dom";
 const User=()=>{
     const params=useParams();
     const {name}=params;
-    // console.log(name);
+    const location=useLocation();
+    console.log(location);
     return(
         <>
             <h1>This is {name} Page</h1>
