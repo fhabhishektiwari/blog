f64524b06b706d33f79185c789f96c0e08fbc92a
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-20 11:28:14 +0530

    Previous Props with Hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.css'
-import React,{useState} from 'react';
-
+import React from 'react';
+import UserSeven from './UserSeven';
 const App=()=>{
-  const [count,setCount]=useState(1);
+  const [count,setCount]=React.useState(0);
   return(
     <div className='App'>
-    <h1>Previous state in functional component</h1>
-    <h3>Count: {count}</h3>
+    <h1>Previous Props with Hooks</h1>
+    <UserSeven count={count}/>
+    
     <button onClick={()=>{
-      // let rand=Math.floor(Math.random()*10);
-      // setCount((prevVal)=>{
-      //   // console.log(prevVal);
-      //   if(prevVal<5){
-      //     alert("value is less than 5");
-      //   }
-      //   return rand;
-      // });
-
-      for(let i=0;i<5;i++){
-        // setCount(count+1);
-        setCount((prevVal)=>{
-          return prevVal+1;
-        })
-      }
-    }}>update</button>
+      setCount(Math.floor(Math.random()*10));
+    }}>Update</button>
 
     </div>
   );
